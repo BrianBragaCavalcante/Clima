5c77090a98828dbfecf6ff15d4e6d7c4c04687f7
Brian Braga Cavalcante <brianbragacavalcantex@gmail.com>
2024-05-09 12:17:25 -0300

    projeto completamente finalizado

--- js/scripts.js
+++ js/scripts.js
@@ -6,8 +6,7 @@ function mudarFavicon(url) {
     if (!favicon.parentNode) {
         document.getElementsByTagName('head')[0].appendChild(favicon);}}
 function atualizarDados(dados) {
-    try {
-        selectClass = document.querySelector
+       selectClass = document.querySelector
         img = `https://openweathermap.org/img/wn/${dados["weather"][0]["icon"]}.png`
         selectClass(".cidade").innerHTML = `Tempo em ${dados["name"]}`
         selectClass(".temp").innerHTML = `${Math.floor(dados["main"]["temp"])}°C`
@@ -15,8 +14,6 @@ function atualizarDados(dados) {
         selectClass(".img-previsao").src = img
         selectClass(".umidade").innerHTML = `Umidade: ${dados["main"]["humidity"]}%`
         mudarFavicon(img)}
-    catch (error) {
-        document.querySelector(".cidade").innerHTML = `Cidade não encontrada`;}}
 async function buscarCidade(cidade) {
     const key = "cebcd482eda57fa9a6714c1c2ba91885"
     const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then(resposta => resposta.json())
